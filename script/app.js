aa8e7eb66d1a6cfb236311c2af8023e6f90aae81
Olga Chirkova <obornola@gmai.com>
2022-11-05 09:49:10 +0300

    добавила функции на кнопки Старт и Финиш

--- script/app.js
+++ script/app.js
@@ -1,8 +1,20 @@
-import {start, pause, reset} from './timer.js';
-import {save} from './saveData.js';
+import {
+    start,
+    pause,
+    reset
+} from './timer.js';
+import {
+    save
+} from './saveData.js';
 
 const buttonStart = document.querySelector('.start');
 buttonStart.addEventListener('click', start);
+
+// function startWork() {
+//     start();
+//     onWorkStart();
+//     drawWorkTimeChart();
+// }
 
 const buttonPause = document.querySelector('.pause');
 buttonPause.addEventListener('click', pause);
@@ -13,6 +25,5 @@ buttonReset.addEventListener('click', reset);
 const buttonSave = document.querySelector('.save');
 buttonSave.addEventListener('click', save);
 
-
-
-
+// const buttonFinish = document.querySelector('.end-of-work');
+// buttonFinish.addEventListener('click', onWorkStop);
